99278f3e698732b4e0c98bb7ec9edb7ff88f739e
Tiago Lang <lang@cyberservices.com>
2019-12-12 16:51:54 +0000

    aula 15

---
+++ src/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace CursoCSharp.Fundamentos
+{
+	class PrimeiroPrograma
+	{
+		public static void Executar(){
+			Console.Write("Primeiro ");
+			Console.WriteLine("programa");
+			Console.WriteLine("terminou");
+		}
+	}
+}
